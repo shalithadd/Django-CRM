7ae48c3638f893e0ad95b3242230d0a5d835033d
Shalitha <shalitha.dehigaspitiyage@gmail.com>
2024-03-26 22:08:13 +0000

    added func to view records and delete records

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-class SighUpForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(
         label="",
         widget=forms.TextInput(
@@ -35,3 +35,30 @@ class SighUpForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control"
+        self.fields["username"].widget.attrs["placeholder"] = "User Name"
+        self.fields["username"].label = ""
+        self.fields["username"].help_text = (
+            '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+        )
+
+        self.fields["password1"].widget.attrs["class"] = "form-control"
+        self.fields["password1"].widget.attrs["placeholder"] = "Password"
+        self.fields["password1"].label = ""
+        self.fields["password1"].help_text = (
+            "<ul class=\"form-text text-muted small\"><li>Your password can't be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can't be a commonly used password.</li><li>Your password can't be entirely numeric.</li></ul>"
+        )
+
+        self.fields["password2"].widget.attrs["class"] = "form-control"
+        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
+        self.fields["password2"].label = ""
+        self.fields["password2"].help_text = (
+            '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+        )
+
+
+# Create Add record form
--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == "POST":
         username = request.POST["username"]
@@ -20,7 +24,7 @@ def home(request):
             )
             return redirect("home")
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records": records})
 
 
 def logout_user(request):
@@ -30,4 +34,43 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered. Welcome!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.success(request, "You must be logged in to view this page.")
+        return redirect("home")
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully...")
+        return redirect("home")
+    else:
+        messages.success(request, "You must be logged in to do this.")
+        return redirect("home")
+
+
+def add_record(request):
+    return render(request, "add_record.html", {})
