cdd6f5424012778ec98d5213c88e946725c75f07
Shalitha <shalitha.dehigaspitiyage@gmail.com>
2024-03-28 23:07:32 +0000

    project complete

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -62,3 +63,57 @@ class SignUpForm(UserCreationForm):
 
 
 # Create Add record form
+class AddRecordFrom(forms.ModelForm):
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "First Name", "class": "form-control"}
+        ),
+        label="",
+    )
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Last Name", "class": "form-control"}
+        ),
+        label="",
+    )
+    email = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Email", "class": "form-control"}
+        ),
+        label="",
+    )
+    phone = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Phone", "class": "form-control"}
+        ),
+        label="",
+    )
+    address = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Address", "class": "form-control"}
+        ),
+        label="",
+    )
+    city = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "City", "class": "form-control"}
+        ),
+        label="",
+    )
+    postcode = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Post Code", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordFrom
 from .models import Record
 
 
@@ -73,4 +73,29 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-    return render(request, "add_record.html", {})
+    form = AddRecordFrom(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect("home")
+
+        return render(request, "add_record.html", {"form": form})
+    else:
+        messages.success(request, "You must be logged in...")
+        return redirect("home")
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordFrom(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Updated...")
+            return redirect("home")
+        return render(request, "update_record.html", {"form": form})
+    else:
+        messages.success(request, "You must be logged in...")
+        return redirect("home")
